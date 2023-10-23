46b31a954e04ee3ea5236df03ab3c7ce35d008ea
Anna <77583972+anna-kireeshka@users.noreply.github.com>
2023-10-23 19:31:45 +0300

    fix () Docker and docker-compose.yml

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import reportWebVitals from "./reportWebVitals";
-import { store } from "./store/store";
-import { Provider } from "react-redux";
+import {store} from "./store/store";
+import {Provider} from "react-redux";
 import App from "./App";
 import '../src/shared/i18n';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-        <App />
-    </Provider>
-  </React.StrictMode>
-  
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
 );
-
-reportWebVitals(console.log);
