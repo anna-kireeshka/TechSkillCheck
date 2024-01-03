fa1ea5a4dbc9f32f4b50123a78199bee39f535ef
Anna <77583972+anna-kireeshka@users.noreply.github.com>
2024-01-04 00:52:20 +0200

    fix  dockerFile env sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Create config.js with runtime environment variables
 echo "window.REACT_APP_API = '${REACT_APP_API}';" > /app/config.js
-echo "window.REACT_APP_RECAPCHA_SECRET_KEY = '${REACT_APP_RECAPCHA_SECRET_KEY}';" > /app/config.js
-echo "window.REACT_APP_RECAPCHA_SITE_KEY = '${REACT_APP_RECAPCHA_SITE_KEY}';" > /app/config.js
+echo "window.REACT_APP_RECAPCHA_SECRET_KEY = '${REACT_APP_RECAPCHA_SECRET_KEY}';" >> /app/config.js
+echo "window.REACT_APP_RECAPCHA_SITE_KEY = '${REACT_APP_RECAPCHA_SITE_KEY}';" >> /app/config.js
 
 # Start the serve command
 exec serve -s -l 8080 app
